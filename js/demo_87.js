3eb79b977e1a86f9a7c66f778d0c7386bc2a5262
vicwu0209 <asd885151@gmail.com>
2022-12-28 19:53:52 +0800

    github commits w11~17

--- js/demo_87.js
+++ js/demo_87.js
@@ -8,14 +8,58 @@ switch(index) {
         p.innerHTML = `<iframe src="./demo/w01_87/index.html" width="100%" height="100%" />`
         break;
     case 2:
-        p.innerHTML = `<iframe src="./demo/w02_87/card_11.html" width="100%" height="100%" />`
+        p.innerHTML = `<iframe src="./demo/w02_87/card_87.html" width="100%" height="100%" />`
         break;
     case 3:
-        p.innerHTML = `<iframe src="./demo/w04_87/blog_11.html" width="100%" height="100%" />`
+        p.innerHTML = `<iframe src="./demo/w04_87/blog_87.html" width="100%" height="100%" />`
         break;
     case 4:
-        p.innerHTML = `<iframe src="./demo/w06_87/blog_11.html" width="100%" height="100%" />`
+        p.innerHTML = `<iframe src="./demo/w06_87/blog_87.html" width="100%" height="100%" />`
         break;
+    case 5:
+        p.innerHTML = `<iframe src="./demo/w08_87/quiz1_11.html" width="100%" height="100%" />`
+        break;
+    case 6:
+        p.innerHTML = `<iframe src="./demo/w09_87/multipage_87.html" width="100%" height="100%" />`
+        break;
+    case 7:
+        p.innerHTML = `<iframe src="./demo/w11_87_midtem/p4_87/p4_87.html" width="100%" height="100%" />`
+        break;
+    case 8:
+        p.innerHTML = `<iframe src="./demo/w12_87_portfolio/portfolio/index.html" width="100%" height="100%" />`
+        break;
+    case 9:
+        p.innerHTML = `<iframe src="./demo/w13_87_portfolio/portfolio/index.html" width="100%" height="100%" />`
+        break;
+    case 10:
+        p.innerHTML = `<iframe src="./demo/w14_87_portfolio/portfolio/index.html" width="100%" height="100%" />`
+        break;
+    case 11:
+        p.innerHTML = `<iframe src="./demo/w15_87_portfolio/portfolio/index.html" width="100%" height="100%" />`
+        break;
+    case 12:
+        p.innerHTML = `<iframe src="./demo/w16_87_portfolio/portfolio/index.html" width="100%" height="100%" />`
+        break;
+    case 13:
+        p.innerHTML = `<iframe src="./works_87.html" width="100%" height="100%" />`
+        break;
+    case 14:
+        p.innerHTML = `<iframe src="./resume_87.html" width="100%" height="100%" />`
+        break;
+    case 15:
+        p.innerHTML = `<iframe src="./learning_87.html" width="100%" height="100%" />`
+        break;
+
+    }
 }
 
-}
+const navbar = document.querySelector('#navbar')
+
+window.addEventListener('scroll', function () {
+
+    if (this.window.pageYOffset > 100) {
+        navbar.classList.add('navbar-fixed');
+    } else {
+        navbar.classList.remove('navbar-fixed');
+    }
+})
